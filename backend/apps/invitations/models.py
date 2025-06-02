cb311520a0f0707783c0054bf1dd9cf45d9751d0
Dave Mcsavvy <davemcsavvii@gmail.com>
2025-06-02 16:53:44 +0100

    feat(migrations): Add new migrations for attendance and invitation models
    - Created a migration to alter model options for the attendance and class models, setting ordering and verbose names.
    - Added a migration to introduce a foreign key field for cohort in the invitation model and updated the program_type field with specific choices.

--- backend/apps/invitations/models.py
+++ backend/apps/invitations/models.py
@@ -1,16 +1,38 @@
 import uuid
 from django.db import models
-
+from django.utils import timezone
 from apps.users.models import User
+from apps.cohorts.models import Cohort
+
 
 class Invitation(models.Model):
     email = models.EmailField()
     role = models.CharField(max_length=20, choices=User.ROLES)
-    program_type = models.CharField(max_length=20, choices=User.PROGRAM_TYPES, null=True)
     token = models.UUIDField(default=uuid.uuid4, unique=True)
     expires_at = models.DateTimeField()
     used_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    program_type = models.CharField(
+        max_length=20,
+        choices=User.PROGRAM_TYPES,
+        null=True,
+        help_text="Only required for students.",
+    )
+    cohort = models.ForeignKey(
+        Cohort,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        help_text="Only required for students.",
+    )
 
     def __str__(self):
         return f"{self.email} - {self.role} - {self.program_type}"
+
+    @property
+    def is_expired(self) -> bool:
+        return self.expires_at < timezone.now()
+
+    @property
+    def is_used(self) -> bool:
+        return self.used_at is not None
