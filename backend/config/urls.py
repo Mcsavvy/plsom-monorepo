e5833f59a7a4a75e6d4568a978bfc03a91296aa8
Dave Mcsavvy <davemcsavvii@gmail.com>
2025-07-13 18:58:32 +0100

    feat: Add user and core API routes to URL configuration
    - Included new URL patterns for user management and core functionalities by adding routes for `api/users/` and `api/`.
    - Improved organization of API endpoints in the URL configuration for better clarity and structure.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,8 +5,14 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("schema", SpectacularAPIView.as_view(), name="schema"),
-    path("docs", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path(
+        "docs",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("api/auth/", include("apps.authentication.urls")),
     path("api/invitations/", include("apps.invitations.urls")),
+    path("api/users/", include("apps.users.urls")),
+    path("api/", include("apps.core.urls")),
     path("", admin.site.urls),
 ]
