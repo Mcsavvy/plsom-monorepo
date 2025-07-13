04d5e449819e99d6c1907d9149e993c1a97e2048
Dave Mcsavvy <davemcsavvii@gmail.com>
2025-07-13 17:00:08 +0100

    chore: Rename communications to core

--- backend/apps/core/apps.py
+++ backend/apps/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CommunicationsConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.communications"
+    name = "apps.core"
