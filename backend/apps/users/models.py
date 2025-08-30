774c932382dc08e044c7058e27afad82a33425b8
Dave Mcsavvy <davemcsavvii@gmail.com>
2025-08-30 12:08:56 +0100

    feat: Allow blank and null values for user role field
    - Updated the 'role' field in the User model to accept blank and null values, enhancing flexibility in user role assignment.
    - Added a migration to reflect this change in the database schema.

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -61,7 +61,7 @@ class User(AbstractUser):
     title = models.CharField(
         max_length=20, choices=TITLES, null=True, blank=True
     )
-    role = models.CharField(max_length=20, choices=ROLES)
+    role = models.CharField(max_length=20, choices=ROLES, blank=True, null=True)
     whatsapp_number = models.CharField(max_length=20, blank=True)
     profile_picture = models.ImageField(
         upload_to="profiles/", null=True, blank=True
